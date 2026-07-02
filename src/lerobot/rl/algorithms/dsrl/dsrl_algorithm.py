60472aca13c67ab41d823742404f12c96e1b9d2a
sezan92 <sezan92@gmail.com>
2026-07-02 21:00:55 +0900

    WIP, complete make optimizers and scheduler

--- src/lerobot/rl/algorithms/dsrl/dsrl_algorithm.py
+++ src/lerobot/rl/algorithms/dsrl/dsrl_algorithm.py
@@ -1,10 +1,34 @@
+import torch
 from ..base import RLAlgorithm
 from .configuratuin_dsrl import DSRLAlgorithmConfig
+
+
 class DSRLAlgorithm(RLAlgorithm):
     config_class = DSRLAlgorithmConfig
-    name="dsrl"
-    def __init__(self, policy, DSRLPolicy, config: DSRLAlgorithmConfig):
+    name = "dsrl"
+
+    def __init__(self, policy: DSRLPolicy, config: DSRLAlgorithmConfig):
         self.config = config
         self.policy = policy
         self.optimizer = dict[str, Optimizer] = {}
-        ## TODO Sezan; complete
+        self._optimization_step: int = 0
+    
+    def make_optimizers_and_scheduler(self):
+        cfg = self.config
+        self.optimizers = {
+            "critic_action": torch.optim.Adam(
+                self.policy.action_critic_ensamble.parameters(), lr=cfg.critic_lr),
+            "critic_noise": torch.optim.Adam(
+                self.policy.noise_critic.parameters(), lr=cfg.critic_lr,
+            ),
+            "noise_actor": torch.optim.Adam(
+                self.policy.noise_actor.parameters(), lr=cfg.critc_lr,
+            ),
+            "temperature": torch.optim.Adam(
+                [self.policy.log_alpha], lr=cfg.temperature_lr
+            ),
+        }
+    def update(self, batch_iterator: Iterator[BatchType]) -> TrainingStats:
+        pass
+    # TODO: sezan: complete
+
