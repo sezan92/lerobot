2be5254f0dacfb3fcfc989b056502140e70b103e
sezan92 <sezan92@gmail.com>
2026-07-13 18:40:36 +0900

    update method

--- src/lerobot/rl/algorithms/dsrl/dsrl_algorithm.py
+++ src/lerobot/rl/algorithms/dsrl/dsrl_algorithm.py
@@ -1,4 +1,5 @@
 import torch
+
 from ..base import RLAlgorithm
 from .configuratuin_dsrl import DSRLAlgorithmConfig
 
@@ -12,23 +13,30 @@ class DSRLAlgorithm(RLAlgorithm):
         self.policy = policy
         self.optimizer = dict[str, Optimizer] = {}
         self._optimization_step: int = 0
-    
+
     def make_optimizers_and_scheduler(self):
         cfg = self.config
         self.optimizers = {
             "critic_action": torch.optim.Adam(
-                self.policy.action_critic_ensamble.parameters(), lr=cfg.critic_lr),
+                self.policy.action_critic_ensamble.parameters(), lr=cfg.critic_lr
+            ),
             "critic_noise": torch.optim.Adam(
-                self.policy.noise_critic.parameters(), lr=cfg.critic_lr,
+                self.policy.noise_critic.parameters(),
+                lr=cfg.critic_lr,
             ),
             "noise_actor": torch.optim.Adam(
-                self.policy.noise_actor.parameters(), lr=cfg.critc_lr,
+                self.policy.noise_actor.parameters(),
+                lr=cfg.critc_lr,
             ),
-            "temperature": torch.optim.Adam(
-                [self.policy.log_alpha], lr=cfg.temperature_lr
-            ),
+            "temperature": torch.optim.Adam([self.policy.log_alpha], lr=cfg.temperature_lr),
         }
-    def update(self, batch_iterator: Iterator[BatchType]) -> TrainingStats:
-        pass
-    # TODO: sezan: complete
 
+    def update(self, batch_iterator: Iterator[BatchType]) -> TrainingStats:
+        clip = self.config.grad_clip_norm # sezan: why?
+        batch = next(batch_iterator)
+        fb = self._prepare_forward_batch(batch)
+
+        # Phase 1: Actor Critic Update (TD-learning)
+        loss_dict = self.policy.forward(fb, model="critic_action")
+        # Update 2026/07/13
+    # TODO: sezan: complete
